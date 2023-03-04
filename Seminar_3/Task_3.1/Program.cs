08e307381fb43f9f68e57d8ff168e706727f55ce
Shpchkn <9516812054@mail.ru>
2023-03-04 15:00:19 +0300

    Task 3.1 completed

--- Seminar_3/Task_3.1/Program.cs
+++ Seminar_3/Task_3.1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 1
+Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом. Проверка без применения строк.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+void palindrom()
+        {
+        Console.Write("Введите 5-ти значное число: ");
+        int number = Convert.ToInt32(Console.ReadLine()); 
+        int num1 = number / 10000 % 10;
+        int num2 = number / 1000 % 10;
+        int rev1 = number / 10 % 10;
+        int rev2 = number % 10;
+        int nul = number / 10000;
+        
+            if (nul < 1 || nul > 9){
+            Console.WriteLine(number + " -> " + "не 5-ти значное число");
+            }
+            else if  (num1 == rev2 && num2 == rev1){
+            Console.WriteLine(number + " -> Да, это палиндром");
+            }
+            else{
+            Console.WriteLine(number + " -> Нет, это не палиндром");
+            }
+        }
+palindrom();

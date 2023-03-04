2dfb6c0807e914939bf2eaad65f7d20f6c04507d
Shpchkn <9516812054@mail.ru>
2023-03-04 13:43:12 +0300

    Task 2.2 completed

--- Seminar_2/Task_2.2/Program.cs
+++ Seminar_2/Task_2.2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberString = Convert.ToString(anyNumber);
+if (anyNumberString.Length > 2){
+  Console.WriteLine("третья цифра: " + anyNumberString[2]);
+}
+else {
+  Console.WriteLine("третьей цифры нет");
+}
